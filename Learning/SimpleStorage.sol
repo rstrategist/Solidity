c3048bf8072bb9a59fd124552a4a522620e45a26
rstrategist <rstrategist@users.noreply.github.com>
2024-01-15 00:36:46 +0100

    Ganache and Web3.py
    - Added Ganache interaction.
    - Interaction with Python and Web3.py

--- Learning/SimpleStorage.sol
+++ Learning/SimpleStorage.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.6.0;
 
 contract SimpleStorage {
-
     /* This will get initialised to 0!
     public variables are also view functions
     without defining as public, it is initialised
@@ -17,22 +16,22 @@ contract SimpleStorage {
         uint256 favNum;
         string name;
     }
-    
+
     // Create a dynamic array of type People[]
     People[] public people;
-
 
     // Dictionary like structure with key-value pairs
     mapping(string => uint256) public nameToFavNum;
 
     // store a number
-    function store(uint256 _favNum) public {
-    favNum = _favNum;
+    function store(uint256 _favNum) public returns (uint256) {
+        favNum = _favNum;
+        return favNum;
     }
 
     // view (read state from the blockchain)
     // pure functions perform some kind of computation
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         //return people[0].favNum;
         return favNum;
     }
@@ -45,5 +44,4 @@ contract SimpleStorage {
         people.push(People(_favNum, _name));
         nameToFavNum[_name] = _favNum;
     }
-    
 }
--- Learning/web3_py_simple_storage/deploy.py
+++ Learning/web3_py_simple_storage/deploy.py
@@ -1,21 +1,99 @@
-from solcx import compile_standard
+""" Compile Solidity contracts using
+the JSON-input-output interface.
+"""
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
 
+# Import .env files
+load_dotenv()
+
+# Read Solidity source code from file
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    # print(simple_storage_file)
 
-# Compile our Solidity code
-
-compiled_sol = sompile_standard{
-    {"language": "Solidity",
-     "sources": {"simple_storage.sol": {"content": simple_storage_file}},
-     "settings": {
-         "outputSelection": {
-             "*" : {"*": ["abi", "metadata", "evm.btecode", "evm.sourceMap"]}
-         }
-     }
-                 
-    }
+# Compile Solidity code
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
     solc_version="0.6.0",
-}
-print(compiled_sol)
+)
+
+# print(compiled_sol)
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# Get bytecode from the JSON
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# Get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# Connecting to Ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+
+# This is a simulated account on an EVM, sorry, no real ETH there :)
+my_address = "0x20218c928aBB3055c3774fAC645f73c8e548A0c8"
+private_key = os.getenv("PRIVATE_KEY")
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# print(SimpleStorage)
+
+# Get the latest transaction
+nonce = w3.eth.get_transaction_count(my_address)
+
+# 1. Build the transaction
+transaction = SimpleStorage.constructor().build_transaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+
+# 2. Sign the transaction
+print("Deploying contract...")
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+# print(signed_txn)
+
+# 3. Send the transaction
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Contract deployed!")
+
+# Working with the contract, you always need:
+# (i) Contract Address
+# (ii) Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call => Simulate making the call and getting a return value
+# Transact => Actually make a state change (transaction) that will be executed on the blockchain
+print(simple_storage.functions.retrieve().call())
+
+# Store a value on the blockchain and retrieve it
+print("Updating contract...")
+# 1. Build transaction
+store_transaction = simple_storage.functions.store(15).build_transaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+# 2. Sign the transaction
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+# 3. Send the transaction
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Contract updated!")
+
+# Retrieve the data to check it was stored correctly
+print(simple_storage.functions.retrieve().call())
