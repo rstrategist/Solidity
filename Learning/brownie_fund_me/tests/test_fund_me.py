a1a14395b6b838f01e1b1db2a42501778f244c00
rstrategist <rstrategist@users.noreply.github.com>
2024-01-23 21:53:06 +0100

    fund_and_withdraw
    - Added fund and withdraw functions.
    - Started to deploy tests.

---
+++ Learning/brownie_fund_me/tests/test_fund_me.py
@@ -0,0 +1,18 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+from scripts.deploy import deploy_fund_me
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee()
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+
+    print(f"The current entry fee is {entrance_fee}.")
+    print("Funding")
+    
