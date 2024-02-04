6fc2c141296ed6a65f3ddbb19d7e6b8e197c646d
rstrategist <rstrategist@users.noreply.github.com>
2024-02-04 20:44:52 +0100

    Added Deployment Script, VRF and LINK Token
    - Deploy contract script sets up price feed, LINK token and VRF coordinator for Lottery contract deployment.
    - Helper functions added:
     - get_contract()
     - deploy_mocks()
    - Config file updated for local Ganache and Sepolia Testnet; ETH/USD price feed and LINK token (address, keyhash and fee)

---
+++ Learning/smartcontract-lottery/scripts/helpful_scripts.py
@@ -0,0 +1,79 @@
+# Helper functions
+from brownie import (
+    network,
+    config,
+    accounts,
+    MockV3Aggregator,
+    Contract,
+    VRFCoordinatorMock,
+    LinkToken)
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "ganache-cli"]
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
+
+def get_account(index=None, id=None):
+    # 1. account[0]
+    # 2. account.add("env")
+    # 3. accouns.load("id") # brownie accounts list
+
+    if index:
+        return accounts[index]
+    
+    if id:
+        return accounts.load(id)
+
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    
+    return accounts.add(config["wallets"]["from_key"])
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+    }
+
+def get_contract(contract_name):
+    """ This function will get the contract addresses from the brownie config
+    if defined, otherwise it will deploy a mock version of that contract, and
+    return that mock contract.
+
+        Args:
+            contract_name (string)
+
+        Returns:
+            brownie.network.ProjectContract: The most recently deployed
+            version of this contract.
+    """
+
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            # MockV3Aggregator.length
+            deploy_mocks()
+        # Most recently deployed version of MockV3Aggregator[-1]
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+            )
+        # MockV3Aggregator.abi
+    return contract
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": get_account()})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Mocks Deployed!")
+
