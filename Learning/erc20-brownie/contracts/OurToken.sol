e663b3cfa31fd5b86137ced34228572f94a1d6a7
rstrategist <rstrategist@users.noreply.github.com>
2024-02-21 07:48:32 +0100

    ERC20 Token Deployment
    Using OpenZeppelin template for ERC20 token deployment.
    - Local Ganache.
    - Sepolia Testnet.

--- Learning/erc20-brownie/contracts/OurToken.sol
+++ Learning/erc20-brownie/contracts/OurToken.sol
@@ -1,17 +1,12 @@
 // contracts/OurToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("OurToken", "OTK")
-        Ownable(initialOwner)
-    {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
     }
 }
--- Learning/erc20-brownie/scripts/1_deploy_token.py
+++ Learning/erc20-brownie/scripts/1_deploy_token.py
@@ -1,5 +1,5 @@
 from brownie import OurToken
-from scripts.helpful_scripts get_account
+from scripts.helpful_scripts import get_account
 from web3 import Web3
 
 initial_supply = Web3.toWei(1000, "ether")
